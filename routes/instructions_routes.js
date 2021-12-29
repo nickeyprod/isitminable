6af3db765727f34659ca49fd3f73eb9489edb69d
= <=>
2021-12-29 17:09:43 +0300

    Fix project name starting with lower case in url

--- routes/instructions_routes.js
+++ routes/instructions_routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    const coin = req.query.coin.toString();
+    const coin = req.query.coin.toString().toLowerCase();
     res.render(`instructions/${coin}`, {title: `Майнинг ${coin[0].toUpperCase() + coin.substring(1, coin.length)}`});
 });
   
