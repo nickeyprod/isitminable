e4750005271f405966afa7711118ece0a99608f3
= <=>
2021-12-29 20:51:29 +0300

    Added Pi Network Project

--- routes/instructions_routes.js
+++ routes/instructions_routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    const coin = req.query.coin.toString().toLowerCase();
+    const coin = req.query.coin.toString().toLowerCase().replaceAll(' ', '_');
     try {
         res.render(`instructions/${coin}`, {title: `Майнинг ${coin[0].toUpperCase() + coin.substring(1, coin.length)}`});
     } catch (err) {
